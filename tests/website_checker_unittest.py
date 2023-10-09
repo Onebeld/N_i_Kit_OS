0ba102d317e9b6907d711bf7ffece5ade3c1013f
Dmitry Zhutkov <dmitry.zhutkov@gmail.com>
2023-10-09 10:51:51 +0500

    Added the ability to obtain a certificate and a test for it

--- tests/website_checker_unittest.py
+++ tests/website_checker_unittest.py
@@ -5,27 +5,47 @@ import website_checker
 
 
 class MyTestCase(unittest.TestCase):
-    def test_get_website_code_response(self):
-        website_google = "https://google.com"
-        website_youtube = "youtube.com"
-        website_python = "www.python.org"
-        website_rsvpu = "https://rsvpu.ru/programs/bakalavriat"
-        website_http = "http://info.cern.ch/"
+    website_google = "https://google.com"
+    website_youtube = "youtube.com"
+    website_python = "www.python.org"
+    website_rsvpu = "https://rsvpu.ru/programs/bakalavriat"
+    website_http = "http://info.cern.ch/"
 
-        status_code_google = website_checker.get_website_code_response(website_google)
+    def test_get_website_code_response(self):
+        status_code_google = website_checker.get_website_code_response(self.website_google)
         print("Response code for google.com: " + str(status_code_google))
 
-        status_code_youtube = website_checker.get_website_code_response(website_youtube)
+        status_code_youtube = website_checker.get_website_code_response(self.website_youtube)
         print("Response code for youtube.com: " + str(status_code_youtube))
 
-        status_code_python = website_checker.get_website_code_response(website_python)
+        status_code_python = website_checker.get_website_code_response(self.website_python)
         print("Response code for python.org: " + str(status_code_python))
 
-        status_code_rsvpu = website_checker.get_website_code_response(website_rsvpu)
+        status_code_rsvpu = website_checker.get_website_code_response(self.website_rsvpu)
         print("Response code for rsvpu.ru: " + str(status_code_rsvpu))
 
-        status_code_http = website_checker.get_website_code_response(website_http)
+        status_code_http = website_checker.get_website_code_response(self.website_http)
         print("Response code for http site: " + str(status_code_http))
+
+        time.sleep(0.2)
+
+        self.assertTrue(True)
+
+    def test_get_ssl_certificate(self):
+        google_organization = website_checker.get_ssl_certificate(self.website_google)
+        print("Organization name from google.com: " + str(google_organization))
+
+        youtube_organization = website_checker.get_ssl_certificate(self.website_youtube)
+        print("Organization name from youtube.com: " + str(youtube_organization))
+
+        python_organization = website_checker.get_ssl_certificate(self.website_python)
+        print("Organization name from python.com: " + str(python_organization))
+
+        rsvpu_organization = website_checker.get_ssl_certificate(self.website_rsvpu)
+        print("Organization name from rsvpu.com: " + str(rsvpu_organization))
+
+        http_organization = website_checker.get_ssl_certificate(self.website_http)
+        print("Organization name from http: " + str(http_organization))
 
         time.sleep(0.2)
 
--- website_checker.py
+++ website_checker.py
@@ -1,3 +1,7 @@
+import socket
+import ssl
+from urllib.parse import urlparse
+
 import requests
 
 
@@ -8,3 +12,23 @@ def get_website_code_response(website: str):
     request = requests.get(website, verify=False)
 
     return request.status_code.numerator
+
+
+def get_ssl_certificate(website: str):
+    if website.count("https://") == 0 and website.count("http://") == 0:
+        website = "https://" + website
+
+    parsed_uri = urlparse(website)
+
+    try:
+        context = ssl.create_default_context()
+        sock = context.wrap_socket(socket.socket(), server_hostname=parsed_uri.netloc)
+        sock.connect((parsed_uri.netloc, 443))
+        certificate = sock.getpeercert()
+
+        issuer = dict(x[0] for x in certificate['issuer'])
+
+        return issuer
+
+    except:
+        return None
